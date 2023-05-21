9073b09f478131a256ac02dc123ff92814dcdb0f
Gustavo Aparecido da Silva <68440761+GustavoSilva29@users.noreply.github.com>
2023-05-21 18:46:00 -0300

    Update Header.js
    Organizando o codigo!

--- pokedex-react-js-main/pokedex-react-js-main/src/components/Header/Header.js
+++ pokedex-react-js-main/pokedex-react-js-main/src/components/Header/Header.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
-
 import "./styles.css";
 import logo from "../../assets/img/logo.png";
 
