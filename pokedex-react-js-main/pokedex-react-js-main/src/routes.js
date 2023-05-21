7becbed05514739276b5cc612e6e7090f0bea78a
Gustavo Aparecido da Silva <68440761+GustavoSilva29@users.noreply.github.com>
2023-05-21 19:21:31 -0300

    Update routes.js
    Organizando a Route

--- pokedex-react-js-main/pokedex-react-js-main/src/routes.js
+++ pokedex-react-js-main/pokedex-react-js-main/src/routes.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-
 import Home from "./pages/Home/Home";
 import Details from "./pages/Details/Details";
+import FavoritesProvider from "./contexts/Favorites";
 
 function Routes() {
   return (
     <div className="app">
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/:query?" component={Home} />
-          <Route exact path="/details/:name?" component={Details} />
-        </Switch>
+        <FavoritesProvider>
+          <Switch>
+            <Route exact path="/:query?" component={Home} />
+            <Route exact path="/details/:name?" component={Details} />
+            <Route exact path="/src/pages/Favorites" component={FavoritesProvider} />
+            <Route exact path="/src/pages/Favorites/index.js" component={FavoritesProvider} />
+          </Switch>
+        </FavoritesProvider>
       </BrowserRouter>
+     
     </div>
   );
 }
