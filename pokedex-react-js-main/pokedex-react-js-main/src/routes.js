a4f816e4a44fb40ec9b8a73afe2d327677a74392
Gustavo Aparecido da Silva <68440761+GustavoSilva29@users.noreply.github.com>
2023-05-24 13:40:52 -0300

    Update routes.js

--- pokedex-react-js-main/pokedex-react-js-main/src/routes.js
+++ pokedex-react-js-main/pokedex-react-js-main/src/routes.js
@@ -2,27 +2,19 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Details from "./pages/Details/Details";
-import FavoritesProvider from "./contexts/Favorites";
 
 function Routes() {
   return (
     <div className="app">
       {/* Configuração do BrowserRouter para envolver as rotas */}
       <BrowserRouter>
-        {/* Provedor de contexto para os favoritos */}
-        <FavoritesProvider>
-          {/* Definição das rotas */}
-          <Switch>
-            {/* Rota para a página inicial, com um parâmetro de consulta opcional */}
-            <Route exact path="/:query?" component={Home} />
-            {/* Rota para a página de detalhes, com um parâmetro de nome opcional */}
-            <Route exact path="/details/:name?" component={Details} />
-            {/* Rota para a página de favoritos */}
-            <Route exact path="/src/pages/Favorites" component={FavoritesProvider} />
-            {/* Rota alternativa para a página de favoritos */}
-            <Route exact path="/src/pages/Favorites/index.js" component={FavoritesProvider} />
-          </Switch>
-        </FavoritesProvider>
+        {/* Definição das rotas */}
+        <Switch>
+          {/* Rota para a página inicial, com um parâmetro de consulta opcional */}
+          <Route exact path="/:query?" component={Home} />
+          {/* Rota para a página de detalhes, com um parâmetro de nome opcional */}
+          <Route exact path="/details/:name?" component={Details} />
+        </Switch>
       </BrowserRouter>
     </div>
   );
