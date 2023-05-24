061bb15eb4f2c56baadc801f17263281e9175338
Gustavo Aparecido da Silva <68440761+GustavoSilva29@users.noreply.github.com>
2023-05-24 10:39:38 -0300

    Update LoadingDetails.js
    add comentarios.

--- pokedex-react-js-main/pokedex-react-js-main/src/components/Loading/LoadingDetails.js
+++ pokedex-react-js-main/pokedex-react-js-main/src/components/Loading/LoadingDetails.js
@@ -4,42 +4,55 @@ import { Row, Col } from "react-bootstrap";
 import Colors from "../../styles/Colors";
 import "./styles.css";
 
+// Componente de detalhes de carregamento
 const LoadingDetails = () => {
   return (
+    // Linha que contém os detalhes de carregamento
     <Row>
       <Col xs={12} md={6}>
+        {/* Item de carregamento */}
         <div className="loading-item  mb-3 p-3">
+          {/* Configuração do tema e cor do skeleton */}
           <SkeletonTheme color={Colors.brand_dark} highlightColor="#444">
+            {/* Skeleton com altura específica */}
             <Skeleton height={340} />
           </SkeletonTheme>
         </div>
       </Col>
 
       <Col xs={12} md={6}>
+        {/* Item de carregamento com altura específica */}
         <div style={{ height: 120 }} className="loading-item  mb-3 p-3">
           <SkeletonTheme color={Colors.brand_dark} highlightColor="#444">
+            {/* Skeleton com altura específica */}
             <Skeleton height={82} />
           </SkeletonTheme>
         </div>
 
+        {/* Item de carregamento com altura específica */}
         <div style={{ height: 214 }} className="loading-item mb-3 p-3">
           <SkeletonTheme color={Colors.brand_dark} highlightColor="#444">
+            {/* Skeleton com altura específica */}
             <Skeleton height={175} />
           </SkeletonTheme>
         </div>
       </Col>
 
       <Col xs={12}>
+        {/* Item de carregamento com altura específica */}
         <div style={{ height: 290 }} className="loading-item mb-3 p-3">
           <SkeletonTheme color={Colors.brand_dark} highlightColor="#444">
+            {/* Skeleton com altura específica */}
             <Skeleton height={250} />
           </SkeletonTheme>
         </div>
       </Col>
 
       <Col xs={12}>
+        {/* Item de carregamento com altura específica */}
         <div style={{ height: 290 }} className="loading-item mb-3 p-3">
           <SkeletonTheme color={Colors.brand_dark} highlightColor="#444">
+            {/* Skeleton com altura específica */}
             <Skeleton height={250} />
           </SkeletonTheme>
         </div>
