7f338bcf5d5244dc913cc23e90e9559f15e12545
Gustavo Aparecido da Silva <68440761+GustavoSilva29@users.noreply.github.com>
2023-05-24 10:41:59 -0300

    Update Footer.js
    add comentarios

--- pokedex-react-js-main/pokedex-react-js-main/src/components/Others/Footer.js
+++ pokedex-react-js-main/pokedex-react-js-main/src/components/Others/Footer.js
@@ -3,17 +3,23 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import "./styles.css";
 
+// Componente do rodapé
 const Footer = () => {
   return (
+    // Rodapé com margem superior de 5 unidades
     <footer className="mt-5">
       <div className="d-flex justify-content-center align-items-center">
+        {/* Créditos do desenvolvedor */}
         <p className="mr-1 credits-text">Desenvolvido por</p>
+        {/* Link para o perfil do desenvolvedor no Github */}
         <a className="mhq" target="_blank" href="https://github.com/GustavoSilva29">
           Gustavo Silva
         </a>
       </div>
+      {/* Link para ver o código no Github */}
       <a>
         Veja o codigo no Github
+        {/* Ícone do Github */}
         <FontAwesomeIcon className="ml-2" icon={faGithub} />
       </a>
     </footer>
