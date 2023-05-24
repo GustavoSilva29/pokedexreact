ce2f55ae2fcbe447d349b307991bd992c7505ca6
Gustavo Aparecido da Silva <68440761+GustavoSilva29@users.noreply.github.com>
2023-05-24 11:44:32 -0300

    Update utils.js
    add comentario ao codigo.

--- pokedex-react-js-main/pokedex-react-js-main/src/functions/utils.js
+++ pokedex-react-js-main/pokedex-react-js-main/src/functions/utils.js
@@ -1,21 +1,24 @@
+// RemoveAspas - Remove as aspas de uma string
 export function RemoveAspas(val) {
   if (val != null) return val.replace(/[\\"]/g, "");
 }
 
+// GetImageById - Retorna a URL da imagem de um pokemon com base no ID
 export function GetImageById(id) {
   id = id.toString().padStart(3, "0");
   return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id}.png`;
 }
 
+// DetermineGenderRate - Determina a taxa de gênero de um pokemon
 export function DetermineGenderRate(gender) {
   switch (gender) {
     case -1:
-      return "N/A";
+      return "N/A"; // Gênero não aplicável
     case 0:
-      return "0%\u2640 100%\u2642";
+      return "0%\u2640 100%\u2642"; // Gênero exclusivamente masculino
     default:
       const female = Math.round(gender * 1250) / 100;
       const male = 100 - female;
-      return `${female}%\u2640 ${male}%\u2642`;
+      return `${female}%\u2640 ${male}%\u2642`; // Taxa de gênero em porcentagem feminina e masculina
   }
 }
