9aa2b1375e1241bd5c35fba9405b2e132eedb6d7
Gustavo Aparecido da Silva <68440761+GustavoSilva29@users.noreply.github.com>
2023-05-24 13:59:03 -0300

    Delete pokedex-react-js-main directory
    Mudando a pasta raiz do projeto

--- pokedex-react-js-main/pokedex-react-js-main/src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-// Cria uma instância do axios com a base URL da PokeAPI
-const api = axios.create({
-  baseURL: "https://pokeapi.co/api/v2",
-});
-
-export default api;
