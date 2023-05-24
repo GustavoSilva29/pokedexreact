9e2c04368bef734400c4018468a0964f316075de
Gustavo Aparecido da Silva <68440761+GustavoSilva29@users.noreply.github.com>
2023-05-24 12:14:58 -0300

    Update routes.js
    add comentarios

--- pokedex-react-js-main/pokedex-react-js-main/src/routes.js
+++ pokedex-react-js-main/pokedex-react-js-main/src/routes.js
@@ -7,17 +7,23 @@ import FavoritesProvider from "./contexts/Favorites";
 function Routes() {
   return (
     <div className="app">
+      {/* Configuração do BrowserRouter para envolver as rotas */}
       <BrowserRouter>
+        {/* Provedor de contexto para os favoritos */}
         <FavoritesProvider>
+          {/* Definição das rotas */}
           <Switch>
+            {/* Rota para a página inicial, com um parâmetro de consulta opcional */}
             <Route exact path="/:query?" component={Home} />
+            {/* Rota para a página de detalhes, com um parâmetro de nome opcional */}
             <Route exact path="/details/:name?" component={Details} />
+            {/* Rota para a página de favoritos */}
             <Route exact path="/src/pages/Favorites" component={FavoritesProvider} />
+            {/* Rota alternativa para a página de favoritos */}
             <Route exact path="/src/pages/Favorites/index.js" component={FavoritesProvider} />
           </Switch>
         </FavoritesProvider>
       </BrowserRouter>
-     
     </div>
   );
 }
