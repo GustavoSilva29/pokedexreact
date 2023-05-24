0281d937f7c876d346fc54c752f8caa764e4d301
Gustavo Aparecido da Silva <68440761+GustavoSilva29@users.noreply.github.com>
2023-05-24 12:10:53 -0300

    Update index.js
    add comentario.

--- pokedex-react-js-main/pokedex-react-js-main/src/index.js
+++ pokedex-react-js-main/pokedex-react-js-main/src/index.js
@@ -13,3 +13,9 @@ ReactDOM.render(
 );
 
 reportWebVitals();
+
+/*a função ReactDOM.render é chamada para renderizar o componente App dentro do elemento com o ID "root" do documento HTML. 
+O React.StrictMode é usado para envolver o componente App e ativar verificações adicionais de segurança e desempenho no modo de desenvolvimento.
+
+Por fim, a função reportWebVitals é chamada para registrar as métricas de desempenho da aplicação. 
+Isso permite analisar e monitorar o desempenho da aplicação com o auxílio da biblioteca "web-vitals".*/
