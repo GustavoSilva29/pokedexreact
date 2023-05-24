12a9136c88721c56b3b6d106c19b49949b5d06c6
Gustavo Aparecido da Silva <68440761+GustavoSilva29@users.noreply.github.com>
2023-05-24 12:08:10 -0300

    Update App.js
    add comentario

--- pokedex-react-js-main/pokedex-react-js-main/src/App.js
+++ pokedex-react-js-main/pokedex-react-js-main/src/App.js
@@ -10,3 +10,10 @@ function App() {
 }
 
 export default App;
+
+/*Este código define o componente App, que é responsável por renderizar as rotas da aplicação. 
+Dentro do componente, há uma verificação para remover a chamada de console.log em produção. 
+Isso significa que, quando o ambiente de execução for diferente de desenvolvimento, 
+a função console.log será substituída por uma função vazia, removendo assim as saídas de log no console. 
+Em seguida, é retornado o componente Routes, que renderiza as rotas da aplicação. 
+O componente App é exportado como o componente principal da aplicação.*/
