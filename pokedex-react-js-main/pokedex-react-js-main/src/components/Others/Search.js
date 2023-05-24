9be17ce4a58b621d4bd409ea5d813aa6be7bf909
Gustavo Aparecido da Silva <68440761+GustavoSilva29@users.noreply.github.com>
2023-05-24 10:47:11 -0300

    Update Search.js
    add comentario.

--- pokedex-react-js-main/pokedex-react-js-main/src/components/Others/Search.js
+++ pokedex-react-js-main/pokedex-react-js-main/src/components/Others/Search.js
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import "./styles.css";
 
+// Componente de pesquisa
 const Search = ({ history, query }) => {
   const [searchQuery, setSeachQuery] = useState(query || "");
 
@@ -12,24 +13,27 @@ const Search = ({ history, query }) => {
   }, [searchQuery]);
 
   return (
-    
+    // Container da pesquisa com margem inferior de 4 unidades
     <div className="container-search mb-4">
       <Form.Label>Nome ou ID do Pokémon</Form.Label>
       <div className="container-input-btn">
+        {/* Input de pesquisa */}
         <input
           onChange={(e) => setSeachQuery(e.currentTarget.value)}
           value={searchQuery}
           placeholder="Ex. Bulbasaur"
         />
-        {searchQuery != "" && (
+        {/* Botão de limpar pesquisa */}
+        {searchQuery !== "" && (
           <button onClick={() => setSeachQuery("")} className="btn-clear">
             <FontAwesomeIcon icon={faTimes} color={"white"} />
           </button>
         )}
         
+        {/* Botão para ir para a página inicial */}
         <button onClick={() => history.push('/index')} className="btn-custom">
-        Pokedex
-      </button>
+          Pokedex
+        </button>
       </div>
     </div>
   );
